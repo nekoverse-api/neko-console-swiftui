678176a664656c403a7b0744279c08762895dcd5
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-12 09:35:15 -0500

    Initial Commit

---
+++ neko-console/neko_consoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  neko_consoleApp.swift
+//  neko-console
+//
+//  Created by Gary Ascuy on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct neko_consoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
